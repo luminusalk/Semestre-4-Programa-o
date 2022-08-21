444c620d6c97acdda3270f64794f1da9b229757b
luminusalk <103453596+luminusalk@users.noreply.github.com>
2022-08-21 01:34:57 -0300

    Correção final

--- Aula2/Atividade13.java
+++ Aula2/Atividade13.java
@@ -12,12 +12,12 @@ public class atividade13 {
         
         //Exibe as mensagens iniciais com instruções
         JOptionPane.showMessageDialog(null,"Bem vindo a calculadora da média de sua nota!");
-        JOptionPane.showMessageDialog(null,"Digite um valor entre 0 e 10. Poderá digitar 5.5 por exemplo.");
+        JOptionPane.showMessageDialog(null,"Digite um valor entre 0 e 10. Digite a nota sem '.' ou ','.");
        
         //Insere o valor das notas na disciplina.
-        nota1 = Double.parseDouble(JOptionPane.showInputDialog(null,"Insira a nota 01:"));
-        nota2 = Double.parseDouble(JOptionPane.showInputDialog(null,"Insira a nota 02:"));
-        nota3 = Double.parseDouble(JOptionPane.showInputDialog(null,"Insira a nota 03:"));
+        nota1 = Integer.parseInt(JOptionPane.showInputDialog(null,"Insira a nota 01:"));
+        nota2 = Integer.parseInt(JOptionPane.showInputDialog(null,"Insira a nota 02:"));
+        nota3 = Integer.parseInt(JOptionPane.showInputDialog(null,"Insira a nota 03:"));
         
         //Verifica se o usuário digitou as notas corretamente para evitar estouros ou média negativa.
         if(nota1 < 0 || nota2 < 0 || nota3 < 0)
@@ -25,16 +25,16 @@ public class atividade13 {
             JOptionPane.showMessageDialog(null,"Você digitou uma das médias abaixo de 0, tente novamente!");
             return;
         }
-        else  if(nota1 > 10 || nota2 > 10 || nota3 > 10)
+        else  if(nota1 > 100 || nota2 > 100 || nota3 > 100)
         {
             JOptionPane.showMessageDialog(null,"Você digitou uma das médias acima de 100, tente novamente!");
             return;
         }
         
         //Atribui o peso as notas de cada atividade
-        nota1 = ((nota1/10)*pesoTL);
-        nota2 = ((nota2/10)*pesoAS);
-        nota3 = ((nota3/10)*pesoEF);
+        nota1 = ((nota1/100)*pesoTL);
+        nota2 = ((nota2/100)*pesoAS);
+        nota3 = ((nota3/100)*pesoEF);
         
         //Soma os pesos para calcular a média final
         media = nota1+nota2+nota3;
