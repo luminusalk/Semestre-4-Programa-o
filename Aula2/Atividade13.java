267b3245ec96a75f48daecf967052569832a4193
luminusalk <103453596+luminusalk@users.noreply.github.com>
2022-08-21 01:30:22 -0300

    Atualização de variáveis
    Pequenas Correções nos Cálculos

--- Aula2/Atividade13.java
+++ Aula2/Atividade13.java
@@ -1,4 +1,4 @@
-/**
+/*
  *
  * @author Pedro Henrique Torres de Alencar
  * Atividade 13 de Programas e Soluções Computacionais
@@ -7,17 +7,17 @@ import javax.swing.JOptionPane;
 
 public class atividade13 {
     public static void main(String[] args){
-        int nota1, nota2, nota3;
-        double pesoTL = 2.00, pesoAS = 3.00, pesoEF = 5.00, media = 0;
+        double nota1, nota2, nota3, media;
+        double pesoTL = 2.00, pesoAS = 3.00, pesoEF = 5.00;
         
         //Exibe as mensagens iniciais com instruções
         JOptionPane.showMessageDialog(null,"Bem vindo a calculadora da média de sua nota!");
-        JOptionPane.showMessageDialog(null,"Digite um valor entre 0 e 100. OBS: Não use '.' ou ','.");
+        JOptionPane.showMessageDialog(null,"Digite um valor entre 0 e 10. Poderá digitar 5.5 por exemplo.");
        
         //Insere o valor das notas na disciplina.
-        nota1 = Integer.parseInt(JOptionPane.showInputDialog(null,"Insira a nota 01:"));
-        nota2 = Integer.parseInt(JOptionPane.showInputDialog(null,"Insira a nota 02:"));
-        nota3 = Integer.parseInt(JOptionPane.showInputDialog(null,"Insira a nota 03:"));
+        nota1 = Double.parseDouble(JOptionPane.showInputDialog(null,"Insira a nota 01:"));
+        nota2 = Double.parseDouble(JOptionPane.showInputDialog(null,"Insira a nota 02:"));
+        nota3 = Double.parseDouble(JOptionPane.showInputDialog(null,"Insira a nota 03:"));
         
         //Verifica se o usuário digitou as notas corretamente para evitar estouros ou média negativa.
         if(nota1 < 0 || nota2 < 0 || nota3 < 0)
@@ -25,7 +25,7 @@ public class atividade13 {
             JOptionPane.showMessageDialog(null,"Você digitou uma das médias abaixo de 0, tente novamente!");
             return;
         }
-        else  if(nota1 > 100 || nota2 > 100 || nota3 > 100)
+        else  if(nota1 > 10 || nota2 > 10 || nota3 > 10)
         {
             JOptionPane.showMessageDialog(null,"Você digitou uma das médias acima de 100, tente novamente!");
             return;
