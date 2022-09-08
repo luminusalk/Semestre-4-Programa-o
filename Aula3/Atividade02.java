67d50bebde56878d207ed4561f64cd5886c045d9
luminusalk <103453596+luminusalk@users.noreply.github.com>
2022-09-07 23:59:11 -0300

    Create Atividade02.java

---
+++ Aula3/Atividade02.java
@@ -0,0 +1,51 @@
+/*
+ *
+ * @author Pedro Henrique Torres de Alencar
+ * Atividade 2 - Aula 3
+ * Verificar 15 entradas de altura e informar a menor e a maior
+ */
+import javax.swing.JOptionPane;
+
+public class Atividade_Desafio {
+    public static void main(String args[]) {
+        int qnt, i = 0;
+        double x = 0,y = 0,z = 0;
+        
+        //Exibe as mensagens iniciais com instruções
+        JOptionPane.showMessageDialog(null,"Olá, posso analisar as alturas das pessoas e informar a menor e a maior do grupo.");
+        
+        qnt = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite a quantidade de alturas que você deseja analisar:"));
+
+        //insira a quantidade de alturas que será calculada (valor de qnt);
+        while(i < qnt)
+        {
+            y = Double.parseDouble(JOptionPane.showInputDialog(null,"Digite a altura Nº: "+(i+1)+" exemplo (1.50)."));
+            
+            if (y < 0)
+            {
+                JOptionPane.showMessageDialog(null,"Você digitou uma altura negativa e que não existe! Preciso interromper!");
+                return;
+            }
+            
+            if (x == 0 && z == 0)
+            {
+                x = z = y;
+            }
+            else
+            {
+                if (y < x)
+                {
+                    x = y;
+                }
+                else if (y > z)
+                {
+                    z = y;
+                }
+            }
+            i++;
+        }
+        
+        //Exibe a mensagem com as alturas:
+        JOptionPane.showMessageDialog(null,"A menor altura foi: "+x+" e a maior "+z+"!");
+    }
+}
